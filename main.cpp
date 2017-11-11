549683cd354a775f681bed1b1135f2f9077421da
Jan Jongboom <janjongboom@gmail.com>
2017-11-11 17:02:39 +0900

    Fix indentation

--- main.cpp
+++ main.cpp
@@ -14,56 +14,56 @@ void receiveTCP() {
         // recvfrom blocks until there is data
         nsapi_size_or_error_t size = socket.recv(data, 2048);
         if (size <= 0) {
-			if (size == NSAPI_ERROR_WOULD_BLOCK) {  // Would block... that's fine (no data on the line)
-				wait_ms(100);
-				continue;
-			}
+            if (size == NSAPI_ERROR_WOULD_BLOCK) {  // Would block... that's fine (no data on the line)
+                wait_ms(100);
+                continue;
+            }
 
-			printf("Error while receiving data from TCP socket, probably it's closed now? (%d)\r\n", size);
-			// @todo: re-connect
+            printf("Error while receiving data from TCP socket, probably it's closed now? (%d)\r\n", size);
+            // @todo: re-connect
             break;
-		}
+        }
 
-		// turn into valid C string
-		data[size] = '\0';
+        // turn into valid C string
+        data[size] = '\0';
 
         printf("Received %d bytes from TCP socket (%s)\r\n", size, data);
     }
 }
 
 int main() {
-	printf("Welcome to Mbed TCP ping pong\n");
+    printf("Welcome to Mbed TCP ping pong\n");
 
-	NetworkInterface *network = easy_connect(true);
-	if (!network) {
-		printf("Failed to get a network connection... See serial output.\n");
-		return 1;
-	}
+    NetworkInterface *network = easy_connect(true);
+    if (!network) {
+        printf("Failed to get a network connection... See serial output.\n");
+        return 1;
+    }
 
     // Open a TCP socket
     nsapi_error_t rt = socket.open(network);
     if (rt != NSAPI_ERROR_OK) {
         printf("Could not open TCP Socket (%d)\r\n", rt);
         return 1;
-	}
-	printf("Opened a TCP socket\n");
+    }
+    printf("Opened a TCP socket\n");
 
-	socket.set_blocking(false);
-	rt = socket.connect(HOST_IP, HOST_PORT);
-	if (rt != NSAPI_ERROR_OK) {
-		printf("Could not connect TCP socket (%d)\r\n", rt);
-	}
+    socket.set_blocking(false);
+    rt = socket.connect(HOST_IP, HOST_PORT);
+    if (rt != NSAPI_ERROR_OK) {
+        printf("Could not connect TCP socket (%d)\r\n", rt);
+    }
 
-	Thread socket_thread;
-	socket_thread.start(&receiveTCP);
+    Thread socket_thread;
+    socket_thread.start(&receiveTCP);
 
-	while (1) {
-		char buffer[] = "ping";
-		socket.send(buffer, strlen(buffer));
+    while (1) {
+        char buffer[] = "ping";
+        socket.send(buffer, strlen(buffer));
 
-		printf("Sent %d bytes over TCP socket (%s)\n", strlen(buffer), buffer);
-		wait_ms(2000);
-	}
+        printf("Sent %d bytes over TCP socket (%s)\n", strlen(buffer), buffer);
+        wait_ms(2000);
+    }
 
-	wait_ms(osWaitForever);
+    wait_ms(osWaitForever);
 }
